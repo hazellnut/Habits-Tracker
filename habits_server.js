a5922d12986a7d7d3f1eb320f24fd70908b61004
Tom <tom.hazell@simplemotion.com>
2023-12-10 16:19:23 +1100

    habits updating correctly. auto refresh working. toggle completed working with DB

--- habits_server.js
+++ habits_server.js
@@ -6,7 +6,7 @@ const app = express();
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'fgt23m1217',
   database: 'habits'
 });
 
@@ -18,13 +18,91 @@ db.connect(err => {
 
 // Configure Express to parse JSON requests
 app.use(express.json());
+app.use(express.static('public'));
 
-// Define API endpoints
 
-// TODO: Define your API endpoints for creating/modifying/deleting habits, marking habits as complete, resetting habit status, and compiling statistics.
+app.get('/api/habits', (req, res) => {
+  const query = 'SELECT habit_name, habit_desc, frequency, idhabit, habit_done FROM habits INNER JOIN habit_complete ON idhabits=idhabit';
+
+  db.query(query, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: 'Error fetching habits from the database' });
+      return;
+    }
+
+    res.json(results);
+  });
+});
+// API endpoint to create a habit
+app.post('/api/habits', (req, res) => {
+  const { habitName, habitDesc, frequency } = req.body;
+
+  if (!habitName || !habitDesc || !frequency) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  const query = 'INSERT INTO habits (habit_name, habit_desc, frequency) VALUES (?, ?, ?)';
+  db.query(query, [habitName, habitDesc, frequency], (err, result) => {
+    
+    if (err) {
+      console.error('Error creating habit:', err);
+      return res.status(500).json({ error: 'Error creating habit' });
+    }
+    const query_id = result.insertId;
+    const init_success_field = 'INSERT INTO habit_complete (idhabit, habit_done) VALUES (?,?)';
+    db.query(init_success_field,[query_id,false],(err,result)=>{
+      if (err) {
+        console.error('Error adding habit complete');
+        return res.status(500).json({ error: 'Error adding habit complete'});
+      }
+    }); 
+  
+
+
+    // Send a success response
+    return res.status(201).json({ message: 'Habit created successfully', habitId: result.insertId });
+  });
+});
+
+app.put('/api/habitcomplete', (req, res) => {
+  const { habitid, habitcomplete } = req.body;
+
+  if (typeof habitcomplete !== 'boolean') {
+    return res.status(400).json({ error: 'Invalid habit completed. Must be a boolean.' });
+  }
+    // Update habit status in the MySQL database
+    const updateQuery = 'UPDATE habit_complete SET habit_done = ? WHERE idhabit = ?';
+
+    db.query(updateQuery, [habitcomplete, habitid], (err, results) => {
+      if (err) {
+        console.error('Error updating habit status:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+  
+      // Check if the habit was found and updated
+      if (results.affectedRows === 0) {
+        return res.status(404).json({ error: 'Habit not found' });
+      }
+  
+      // Success
+      return res.json({ success: true });
+    });
+});
+
+app.delete('/api/habits', )
+
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+function insert_habit(){
+
+}
+
+
+function add_success_field(id){
+
+}
